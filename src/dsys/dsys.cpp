f29639fed17864875da7971773bccd21a92575a8
John Tsiombikas <nuclear@member.fsf.org>
2004-11-16 20:17:19 +0000

    small fix, undefined scalar_t on dsys.cpp

--- src/dsys/dsys.cpp
+++ src/dsys/dsys.cpp
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "dsys.hpp"
 #include "part.hpp"
 #include "3dengfx/3dengfx.hpp"
+#include "n3dmath2/n3dmath2.hpp"
 #include "common/timer.h"
 #include "script.h"
 #include "common/bstree.hpp"

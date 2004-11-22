fec2e5ca8e92e2fd55f3f0db9b81076521ef1751
Michael Georgoulopoulos <mgeorgoulopoulos@gmail.com>
2004-11-22 22:22:48 +0000

    jpeg ready (at last)

--- src/common/image.c
+++ src/common/image.c
@@ -20,19 +20,39 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <stdio.h>
 #include <stdlib.h>
 #include <png.h>
+#include <jpeglib.h>
 #include "image.h"
 
 #define FILE_SIG_BYTES	8
 
+typedef struct
+{
+    unsigned char r,g,b;
+}RGBTriplet;
+
+// change this macro to support big endian machines
+#define pack32rgb(r,g,b) ((0<<24) | (r<<16) | (g<<8) | b)
+
+/*jpeg signature*/
+int jpeg_sig_cmp(unsigned char* sig){
+    if(sig[0]!=0xff) return 1;
+    if(sig[1]!=0xd8) return 1;
+    if(sig[2]!=0xff) return 1;
+    if(sig[3]!=0xe0) return 1;
+
+    return 0;
+}
+
 /* Local function prototypes */
 static void *load_png(FILE *fp, unsigned long *xsz, unsigned long *ysz);
+static void *load_jpeg(FILE *fp, unsigned long *xsz, unsigned long *ysz);
 
 /* implementation */
 
 void *load_image(const char *fname, unsigned long *xsz, unsigned long *ysz) {
 	FILE *file;
 	unsigned char signature[FILE_SIG_BYTES];
-	
+
 	if(!(file = fopen(fname, "rb"))) {
 		fprintf(stderr, "Image loading error: could not open file %s\n", fname);
 		return 0;
@@ -43,6 +63,9 @@ void *load_image(const char *fname, unsigned long *xsz, unsigned long *ysz) {
 	if(png_sig_cmp(signature, 0, FILE_SIG_BYTES) == 0) {
 		return load_png(file, xsz, ysz);
 	}
+	else
+	if(jpeg_sig_cmp(signature) == 0)
+		return load_jpeg(file, xsz, ysz);
 	
 	return 0;
 }
@@ -116,4 +139,63 @@ void *load_png(FILE *fp, unsigned long *xsz, unsigned long *ysz) {
 	png_destroy_read_struct(&png_ptr, &info_ptr, 0);
 	
 	return pixels;
+}
+
+static void *load_jpeg(FILE *fp, unsigned long *xsz, unsigned long *ysz){
+	int i;
+	RGBTriplet *buffer;
+	unsigned long *image;
+	
+	JSAMPLE *tmp;
+    
+	struct jpeg_decompress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+	cinfo.err = jpeg_std_error(&jerr);
+	
+	fseek(fp , SEEK_SET , 0);
+
+	jpeg_create_decompress(&cinfo);
+	jpeg_stdio_src(&cinfo, fp);
+	
+	jpeg_read_header(&cinfo, TRUE);
+	
+	/* force output to rgb */
+	cinfo.out_color_space = JCS_RGB;
+	
+    /* allocate space */
+    buffer = malloc(cinfo.image_width * sizeof(RGBTriplet));
+    if (!buffer) return 0;
+    image = malloc(cinfo.image_width * (cinfo.image_height) * sizeof(unsigned long));
+    if (!image) return 0;
+    
+    tmp = (JSAMPLE*) buffer;
+
+	/* return w and h */
+	*xsz = cinfo.image_width;
+	*ysz = cinfo.image_height;
+    
+	/* Decompress, pack and store */
+    jpeg_start_decompress(&cinfo);
+    {
+        while (cinfo.output_scanline < cinfo.output_height) 
+        {
+            jpeg_read_scanlines(&cinfo, &tmp, 1);
+			if (cinfo.output_scanline == 0) continue;
+            
+            for (i=0;i<cinfo.image_width;i++)
+            {
+				/* packing to 32 bit. take care for big endian*/
+                image[i+(cinfo.output_scanline-1)*cinfo.output_width]
+                    = pack32rgb(buffer[i].r , buffer[i].g , buffer[i].b);    
+            }
+        }
+    }
+    jpeg_finish_decompress(&cinfo);
+	
+	/*Done - cleanup*/
+	jpeg_destroy_decompress(&cinfo);
+	free(buffer);
+	fclose(fp);
+
+	return (void*) image;
 }

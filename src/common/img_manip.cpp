4424576e13f3b2d7a87913f2f7f208b822806907
John Tsiombikas <nuclear@member.fsf.org>
2004-12-06 23:28:15 +0000

    - made the filenames for the image manipulation sensible - cleaned up the global namespace by making most of the functions static   (of the image manipulation module) - added the GPL notices and a few other comments

--- src/common/img_manip.cpp
+++ src/common/img_manip.cpp
@@ -1,9 +1,31 @@
-// ImageManipulation.cpp
+/*
+Copyright 2004 John Tsiombikas <nuclear@siggraph.org>
 
-#include "ImageManipulation.hpp"
-#include "ImageManipulation.inl"
-#include "malloc.h"
-#include "memory.h"
+This file is part of the 3dengfx, realtime visualization system.
+
+3dengfx is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+3dengfx is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with 3dengfx; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/* image manipulation
+ * author: Mihalis Georgoulopoulos 2004
+ * modified: John Tsiombikas 2004
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "img_manip.hpp"
 
 // Macros
 #define PACK_ARGB32(a,r,g,b)   	((a<<24) | (r<<16) | (g<<8) | b)
@@ -12,7 +34,10 @@
 #define GETG(c) 		((c>> 8) & 0xff)
 #define GETB(c) 		((c    ) & 0xff)
 
-bool ResampleLine(scalar_t *dst, int dst_width, int dst_pitch,
+static inline scalar_t Cerp(scalar_t x0, scalar_t x1, scalar_t x2, scalar_t x3, scalar_t t);
+static inline int ClampInteger(int i, int from, int to);
+
+static bool ResampleLine(scalar_t *dst, int dst_width, int dst_pitch,
 		  scalar_t *src, int src_width, int src_pitch)
 {
 	if (!dst || !src) return false;
@@ -41,7 +66,7 @@ bool ResampleLine(scalar_t *dst, int dst_width, int dst_pitch,
 	return true;
 }
 
-bool Resample2D(scalar_t *dst, int dst_w, int dst_h,
+static bool Resample2D(scalar_t *dst, int dst_w, int dst_h,
  	        scalar_t *src, int src_w, int src_h)
 {
 	if (!src || !dst) return false;
@@ -91,7 +116,7 @@ bool Resample2D(scalar_t *dst, int dst_w, int dst_h,
 	return true;
 }
 
-bool PackScalarRGB2DW(unsigned long *dst, scalar_t *ac, scalar_t *rc, 
+static bool PackScalarRGB2DW(unsigned long *dst, scalar_t *ac, scalar_t *rc, 
 		      scalar_t *gc, scalar_t *bc , int samples)
 {
 	if (!dst || !ac || !rc || !gc || !bc) return false;
@@ -116,11 +141,11 @@ bool PackScalarRGB2DW(unsigned long *dst, scalar_t *ac, scalar_t *rc,
 	return true;
 }
 
-bool ResamplePixelBuffer(PixelBuffer *pb , int w,int h)
+bool ResamplePixelBuffer(PixelBuffer *pb, int w, int h)
 {
 	if (!pb || !pb->buffer || pb->width<0 || pb->height<0) return false;
 
-	if (pb->width == w && pb->height == h) return true;
+	if ((int)pb->width == w && (int)pb->height == h) return true;
 
 	// split channels
 	scalar_t *a,*newa,*r,*newr,*g,*newg,*b,*newb;
@@ -135,7 +160,7 @@ bool ResamplePixelBuffer(PixelBuffer *pb , int w,int h)
 	newg = (scalar_t*) malloc(w*h*sizeof(scalar_t));
 	newb = (scalar_t*) malloc(w*h*sizeof(scalar_t));
 
-	for (int i=0;i<pb->width*pb->height;i++)
+	for(int i=0; i<(int)(pb->width * pb->height); i++)
 	{
 		a[i] = GETA(pb->buffer[i]);
 		r[i] = GETR(pb->buffer[i]);
@@ -165,4 +190,28 @@ bool ResamplePixelBuffer(PixelBuffer *pb , int w,int h)
 }
 
 
-// End of file
+// --- static inline functions ---
+
+static inline scalar_t Cerp(scalar_t x0, scalar_t x1, scalar_t x2, scalar_t x3, scalar_t t)
+{
+	scalar_t a0,a1,a2,a3,t2;
+
+	t2 = t*t;
+	a0 = x3 - x2 - x0 + x1;
+	a1 = x0 - x1 - a0;
+	a2 = x2 - x0;
+	a3 = x1;
+
+	return(a0*t*t2+a1*t2+a2*t+a3);
+}
+
+
+static inline int ClampInteger(int i, int from, int to)
+{
+	int r=i;
+	if (r<from) r=from;
+	if (r>to) r =to;
+
+	return r;
+}
+
---
+++ src/common/img_manip.hpp
@@ -0,0 +1,34 @@
+/*
+Copyright 2004 John Tsiombikas <nuclear@siggraph.org>
+
+This file is part of the 3dengfx, realtime visualization system.
+
+3dengfx is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+3dengfx is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with 3dengfx; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#ifndef _IMG_MANIP_HPP_
+#define _IMG_MANIP_HPP_
+
+/* image manipulation
+ * author: Mihalis Georgoulopoulos 2004
+ * modified: John Tsiombikas 2004
+ */
+
+#include "gfx/pbuffer.hpp"
+#include "3dengfx/3denginefx_types.hpp"
+#include "n3dmath2/n3dmath2_types.hpp"
+
+bool ResamplePixelBuffer(PixelBuffer *pb , int w,int h);
+
+#endif	// _IMG_MANIP_HPP_
